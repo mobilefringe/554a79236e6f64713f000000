c286ea6639d66559b9972f6d52b6d830b9daba17
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2015-11-10 15:42:49 -0500

    Erin Mills 1.1  /application.js

--- application.js
+++ application.js
@@ -266,6 +266,7 @@ function toggle_mobile_menu(){
 
 $(document).ready(function(){
     $(document).bind('render:complete', function() {
+        $('.site_content_container').height($('.side_nav').height());
         $('[rel=lightbox], [rel=lightbox-gallery]').slimbox();
     });
 });
