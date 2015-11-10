e8dc13264bd38aac9d5735d59d3d997d3e4fca72
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2015-11-10 14:28:38 -0500

    Erin Mills 1.1  /promotions_id.js

--- promotions_id.js
+++ promotions_id.js
@@ -33,7 +33,8 @@ $(document).ready(function() {
         var slug = pathArray[pathArray.length-1];
         promo_details = getPromotionDetailsBySlug(slug);
         render_page_details("#promo_container", "#promo_template", promo_details)
-      
+        
+        $('[rel=lightbox]').lightbox();
     }
     
     function render_page_details(container, template, collection){
