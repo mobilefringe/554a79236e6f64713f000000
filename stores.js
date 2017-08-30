2051ec4628e64207a192a17ec297a63fdf06d414
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-30 16:55:34 -0400

    Erin Mills 1.8  /stores.js

--- stores.js
+++ stores.js
@@ -55,10 +55,11 @@
                 var count = 0;
                 
                 $.each( stores , function( i, store ) {
+                    console.log(store.categories)
                     if(store.categories != null){
                         var store_category = store.categories;
                     }
-                    var a = store_category.indexOf(category_id);
+                    var a = store.categories.indexOf(category_id);
                     if(a > -1){
                         if (count == 0){
                             store.show  = "display:block"; 
