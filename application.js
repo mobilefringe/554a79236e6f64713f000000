817b454afd914e6a35dad1cf34db38f10e40e019
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-05-25 13:11:14 -0400

    Erin Mills 1.8  /application.js

--- application.js
+++ application.js
@@ -59,7 +59,7 @@ function renderSideEvents(container, template, collection, type){
     var template_html = $(template).html();
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);
-    console.log(collection)
+    console.log(collection, type)
     if (type=="event"){
         $.each( item_list , function( key, val ) {
             
