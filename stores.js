8ff623d16afa796d039b1d47496b534bebc0e99a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-26 11:49:18 -0400

    Erin Mills 1.9  /stores.js

--- stores.js
+++ stores.js
@@ -154,11 +154,19 @@
             
             
             
+            
+            
+            
             $('#cats_title').text($(this).find('option:selected').text())
             renderCategoriesList(stores_list, Math.round(stores_list.length/2));
             $('.categories_tab').show()
             
             console.log(stores_list)
+            
+            
+            
+            
+            
             
             document.location.hash = '#/category/' + [$(this).val(), encodeURI($(this).find('option:selected').text())].join('/');
         });
