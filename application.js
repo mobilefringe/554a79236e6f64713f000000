2b9e728eec45087949f5127c6f66e4da9359ca0b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-04-02 13:28:11 -0400

    Erin Mills 1.9  /application.js

--- application.js
+++ application.js
@@ -62,6 +62,7 @@ function renderSideEvents(container, template, collection, type){
     console.log("type", type)
     if (type=="event"){
         $.each( item_list , function( key, val ) {
+            console.log("event val", val)
             if ((val.event_image_url).indexOf('missing.png') > -1){
                 val.alt_promo_image_url = "//kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/172a94a0e1dd6a2eeec91e2cea4e8b92/logo.png"
                 if (val.eventable_type == "Store") {
